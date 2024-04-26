2736ef56a6b0bd950d4f70a8791ab81473a43937
haseebkhan9081 <aa9081haseeb@gmail.com>
2024-04-26 21:31:33 +0500

    completed

--- app/components/ total-purchases-per-product-per-year.js
+++ app/components/ total-purchases-per-product-per-year.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 export const TotalPurchasePerProductPerYear = () => {
     const [data, setData] = useState([]);
     
     useEffect(() => {
-        axios.get("/api/total-purchases-per-product-per-year", {
-            headers: {
-                'Cache-Control': 'no-cache'
-            }
-        })
-            .then((response) => {
-                setData(response.data);
-            })
-            .catch((error) => {
+        const fetchData = async () => {
+            try {
+                const timestamp = new Date().getTime(); // Generate a unique timestamp
+                const response = await fetch(`/api/total-purchases-per-product-per-year?_cache=${timestamp}`);
+                const responseData = await response.json();
+                setData(responseData);
+            } catch (error) {
                 console.log("Error retrieving data", error);
-            });
+            }
+        };
+
+        fetchData();
     }, []);
 
     return (
@@ -24,20 +24,24 @@ export const TotalPurchasePerProductPerYear = () => {
                 <h3>1.Total Purchase Per Product Per Year</h3>
             </div>
             <table border="1">
-                <tr>
-                    <th>Product Name</th>
-                    <th>Year</th>
-                    <th>Total Quantity</th>
-                    <th>Total Revenue</th>
-                </tr>
-                {data?.map((stats) => (
-                    <tr key={stats.id}>
-                        <td>{stats.productName}</td>
-                        <td>{stats.year}</td>
-                        <td>{stats.totalQuantity}</td>
-                        <td>{stats.totalRevenue}</td>
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Year</th>
+                        <th>Total Quantity</th>
+                        <th>Total Revenue</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    {data?.map((stats) => (
+                        <tr key={stats.id}>
+                            <td>{stats.productName}</td>
+                            <td>{stats.year}</td>
+                            <td>{stats.totalQuantity}</td>
+                            <td>{stats.totalRevenue}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
     );
